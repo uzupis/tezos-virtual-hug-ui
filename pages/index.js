c5ddb0d425d6e2e18050e4f87816ab41cd30919a
uzupis <84919649+uzupis@users.noreply.github.com>
2024-01-15 14:17:53 +0100

    Update index.js change success message

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ const SuccessAlert = ({ transactionMsg, setIsSuccess }) => {
 					<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
 				</svg>
 				<span class="sr-only">Info</span>
-				<h3 class="text-lg font-medium">Your Hug was Delivered Successfully</h3>
+				<h3 class="text-lg font-medium">Your $HUX were delivered successfully</h3>
 			</div>
 			<div class="mt-2 mb-4 text-sm">
 			</div>
